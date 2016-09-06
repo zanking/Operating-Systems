e6aad048c8bb33fd616cc77c7f0a8560d3579dcd
Nick <Nicholas.clement@colorado.edu>
2016-09-05 18:03:58 -0600

    so close

---
+++ implement.c
@@ -0,0 +1,21 @@
+#define _GNU_SOURCE
+#include <unistd.h>
+#include <sys/syscall.h>
+#include <stdio.h>
+
+/*int simpleadd(int number1, int number2, int *result){
+	*result = number1+number2;
+	return 0;
+}*/
+
+int main(){
+	
+	int number1 = 3;
+	int number2 = 1;
+	int *result=NULL;
+	int x = syscall(326,10,12,&result);//simpleadd(number1,number2, &result);
+	printf("%d\n", *result);
+	printf("%d\n", x);
+	return 0;
+}
+	
