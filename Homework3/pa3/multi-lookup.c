e58cf05834ae7644bec1a03e2fd740627592e3d5
Nick <Nicholas.clement@colorado.edu>
2016-10-07 16:25:25 -0600

    working on init queue

--- Homework3/pa3/multi-lookup.c
+++ Homework3/pa3/multi-lookup.c
@@ -8,43 +8,82 @@
  * 	threaded solotuion to dnslookup
  *
  */
-
  #include <stdlib.h>
  #include <stdio.h>
  #include <string.h>
  #include <errno.h>
 
  #include "util.h"
-
+ #include "queue.h"
  #define MINARGS 3
- #define MAXARGS 11
- #define MAX_RESOLVER_THREADS 10
+ #define MAXARGS 10
+ #define MAX_RESOLVER_THREADS 11 //set to 11 because up to 10 input files
  #define MIN_RESOLVER_THREADS 2
  #define MAX_NAME_LENGTH 1025
+ #define SBUFSIZE 1025
  #define USAGE "<inputFilePath> <outputFilePath>"
  // #define SBUFSIZE 1025
  #define INPUTFS "%1024s"
 
+
+void * openInput(char * argv[], i){
+  inputfp = fopen(argv[i], "r");
+  if(!inputfp){
+      sprintf(errorstr, "Error Opening Input File: %s", argv[i]);
+      perror(errorstr);
+      break;
+  }
+
+  ///////////////////////////////////
+  /* Read File and Process*/
+  while(fscanf(inputfp, INPUTFS, hostname) > 0){
+
+      /* Lookup hostname and get IP string */
+      if(dnslookup(hostname, firstipstr, sizeof(firstipstr))
+         == UTIL_FAILURE){
+    fprintf(stderr, "dnslookup error: %s\n", hostname);
+    strncpy(firstipstr, "", sizeof(firstipstr));
+      }
+
+      /* Write to Output File */
+    queue_push(&q, payload_in[i]), hostname, firstipstr);
+  }
+
+  /* Close Input File */
+  fclose(inputfp);
+    }
+}
+
+
+
+
 int main(int argc, char* argv[]){
 
   /* Local Vars */
-  pthread_t *th1, *th2, *th3;
+  // pthread_t *th1, *th2, *th3;
+  const int qSize = SBUFSIZE; //define the size of our queue
+  numReqThreads = argc - 1; //take the number of command line args and subtract 2 for program name and output name
 
-  FILE* inputfp = NULL;
-  FILE* outputfp = NULL;
+  FILE* inputFile[numReqThreads]; //create an array of pointers the size of the number of INPUTFS
+  pthread_t threads[numReqThreads];
+  FILE* outputfp = NULL;  //single output file
   char hostname[SBUFSIZE];
   char errorstr[SBUFSIZE];
   char firstipstr[INET6_ADDRSTRLEN];
   int i;
-
-  // init the different threads
-
-  th1 = (pthread_t *) malloc(sizeof(pthread_t));
-  if (pthread_create(th1, NULL, odd, (void *)&MAX_NAME_LENGTH)) {
-    perror("error creating the first thread");
-    exit(1);
+  int rc;
+  queue q; //define queue
+  /* Initialize Queue */
+  if(queue_init(&q, qSize) == QUEUE_FAILURE){
+fprintf(stderr,
+  "error: queue_init failed!\n");
   }
-  printf("created the first thread\n");
+
+  /* loop through input args and make threads */
+  for (t = 0; t< numReqThreads; t++){
+    printf("in main creating thread");
+    rc = pthread_create(&(threads[t]), NULL, )
+  }
 
   /* Check Arguments */
   if(argc < MINARGS || argc > MAXARGS){
@@ -60,9 +99,5 @@ return EXIT_FAILURE;
     perror("Error Opening Output File");
     return EXIT_FAILURE;
   }
-
-
-
-
 
 }
